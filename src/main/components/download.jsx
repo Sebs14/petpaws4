6f7c063f3a3da457efa498417b898ccf5a70c691
Sebs14 <00196919@uca.edu.sv>
2022-07-08 20:45:33 -0600

    add new implementation for designe

---
+++ src/main/components/download.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function download() {
+    return(
+        <a href="https://drive.google.com/drive/folders/1YpdaLo56mmpzhNUctjaS4SvPa05d00Ke?usp=sharing"  class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+<span class="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+<span class="relative px-20 py-10 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+<span class="relative text-white text-3xl">Download APK</span>
+</span>
+</a>
+
+    )
+}
